5491f622bd3aeab9abc8f9bcc69e2db97543ec98
caijianwei <120990691@qq.com>
2017-08-04 18:20:26 +0800

    添加tester

--- excel/src/main/java/com/taiga/oak/excel/A.java
+++ excel/src/main/java/com/taiga/oak/excel/A.java
@@ -1,5 +1,11 @@
 package com.taiga.oak.excel;
 
+import java.time.LocalDate;
+
 public class A {
 
+  public static void main(String[] args) {
+
+    System.err.println(LocalDate.ofEpochDay(43321l));
+  }
 }

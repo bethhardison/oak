f04aca55b51c19431dd06cb6f1a78597dce8fdc2
caijianwei <120990691@qq.com>
2017-08-29 16:41:50 +0800

    添加tester

--- tester/src/test/java/com/taiga/oak/tester/HttpTest.java
+++ tester/src/test/java/com/taiga/oak/tester/HttpTest.java
@@ -1,8 +1,12 @@
 package com.taiga.oak.tester;
 
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by CJW on 2017/8/29.
@@ -10,7 +14,17 @@ import java.io.IOException;
 public class HttpTest {
 
   @Test
-  public void test() throws IOException {
-    System.err.println("11");
+  public void test() throws IOException, URISyntaxException {
+    URI uri = new URIBuilder()
+        .setScheme("http")
+        .setHost("www.google.com")
+        .setPath("/search")
+        .setParameter("q", "httpclient")
+        .setParameter("btnG", "Google Search")
+        .setParameter("aq", "f")
+        .setParameter("oq", "")
+        .build();
+    HttpGet httpget = new HttpGet(uri);
+    System.out.println(httpget.getURI());
   }
 }

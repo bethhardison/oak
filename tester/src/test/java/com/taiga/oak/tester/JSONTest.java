fcd926a207e5e40b6f3e75a8752d067e2d7eae11
caijianwei <120990691@qq.com>
2017-08-30 14:32:36 +0800

    添加tester

--- tester/src/test/java/com/taiga/oak/tester/JSONTest.java
+++ tester/src/test/java/com/taiga/oak/tester/JSONTest.java
@@ -9,20 +9,20 @@ import java.util.Map;
 
 public class JSONTest {
 
-	@Test
-	public void test() {
-		JSONObject love = new JSONObject();
-		love.put("我", "青菜");
-		love.put("猫", "鱼");
+  @Test
+  public void test() {
+    JSONObject love = new JSONObject();
+    love.put("我", "吃青菜");
+    love.put("猫", "吃鱼");
 
-		System.out.println(love.toString());
-	}
+    System.out.println(love.toString());
+  }
 
-	@Test
-	public void testMap() {
-		Map<String, Object> map = new HashMap<>();
-		System.err.println(map.put("a", 1));
-		System.err.println(map.put("a", 1));
-	}
+  @Test
+  public void testMap() {
+    Map<String, Object> map = new HashMap<>();
+    System.err.println(map.put("a", 1));
+    System.err.println(map.put("a", 1));
+  }
 
 }

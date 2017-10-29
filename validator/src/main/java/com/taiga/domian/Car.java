45efb583aa846ad515e92012767cbaf77eed6da2
bethhardison <120990691@qq.com>
2017-10-29 23:27:52 +0800

    validator

---
+++ validator/src/main/java/com/taiga/domian/Car.java
@@ -0,0 +1,26 @@
+package com.taiga.domian;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by CJW on 2017/10/29.
+ */
+@Getter
+@Setter
+public class Car {
+
+  @NotNull
+  private String manufacturer;
+
+  @NotNull
+  @Size(min = 2, max = 14)
+  private String licensePlate;
+
+  @Min(2)
+  private int seatCount;
+}
